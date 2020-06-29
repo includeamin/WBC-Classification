962f46e5083569c1c82f16df9988aa528417a432
aminjamal <aminjamal10@gmail.com>
2020-06-29 21:02:42 +0430

    some changes

--- Utils/ImageTools.py
+++
@@ -1,9 +0,0 @@
-from keras.preprocessing.image import img_to_array
-
-
-class ImageToArrayPreprocessor:
-    def __init__(self, data_format=None):
-        self.dataFormat = data_format
-
-    def preprocess(self, image):
-        return img_to_array(image, data_format=self.dataFormat)
